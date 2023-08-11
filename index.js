b5456e2c9ff5f4fa813b9e11a67bc4ab60e24f13
Erik Peteri <peteri.erik@gmail.com>
2023-08-11 14:11:16 +0300

    App backend

---
+++ index.js
@@ -0,0 +1,25 @@
+const PORT = process.env.PORT || 3001
+
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+require('dotenv').config()
+ 
+const app = express()
+
+app.use(cors())
+
+app.get('/weather', async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=${process.env.REACT_APP_API_KEY}`
+    )
+    res.json(response.data)
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' })
+  }
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+})
